3a9c0434459b3dda05b8c1e84e0b4b50d1e6f2d2
TheClashFruit <clash.fruit123@gmail.com>
2022-12-01 18:38:11 +0100

    feat: switch to fragments, add drawer

--- app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
+++ app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
@@ -1,85 +1,72 @@
 package me.theclashfruit.rithle
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.toolbox.JsonObjectRequest
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
+import me.theclashfruit.rithle.fragments.ModsFragment
 
-import me.theclashfruit.rithle.adapters.ModListAdapter
-import me.theclashfruit.rithle.classes.RithleSingleton
-import me.theclashfruit.rithle.models.ModrinthSearchHitsModel
-import me.theclashfruit.rithle.models.ModrinthSearchModel
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var toolBar: MaterialToolbar
 
-class MainActivity : AppCompatActivity() {
-    private var currentIndex = 20
-    private var lastIndex    = 0
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
-    private var allData: ArrayList<ModrinthSearchHitsModel> = arrayListOf();
-    private var recyclerView: RecyclerView? = null;
-
-    private val listAdapter   = ModListAdapter(allData, this)
-    private val layoutManager = LinearLayoutManager(this)
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.recyclerView)
+        appBarLayout = findViewById(R.id.appBarLayout)
+        toolBar      = findViewById(R.id.toolBar)
 
-        recyclerView!!.layoutManager = layoutManager
-        recyclerView!!.adapter       = listAdapter
+        navigationView = findViewById(R.id.navigationView)
+        drawerLayout   = findViewById(R.id.drawerLayout)
 
-        getItems()
+        setSupportActionBar(toolBar)
 
-        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                if(!recyclerView.canScrollVertically(1))
-                    getItems()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
 
-                super.onScrollStateChanged(recyclerView, newState)
-            }
-        })
+        supportActionBar!!.subtitle = "Mods"
+
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.app_name, R.string.app_name)
+
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val mainFragmentTransaction = supportFragmentManager.beginTransaction()
+
+        val modsFragment = ModsFragment.newInstance()
+
+        mainFragmentTransaction
+            .replace(R.id.fragmentContainer, modsFragment)
+            .commit()
     }
 
-    private fun getItems() {
-        Log.d("webCall", "funCalled")
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawerFragmentTransaction = supportFragmentManager.beginTransaction()
 
-        val format = Json { ignoreUnknownKeys = true }
+        when(item.itemId) {
+            R.id.itemMods -> {
+                val modsFragment = ModsFragment.newInstance()
 
-        val jsonObjectRequest = object : JsonObjectRequest(Method.GET, "https://api.modrinth.com/v2/search?limit=${currentIndex}&offset=${lastIndex}&index=relevance&facets=%5B%5B%22categories%3A%27forge%27%22%2C%22categories%3A%27fabric%27%22%2C%22categories%3A%27quilt%27%22%2C%22categories%3A%27liteloader%27%22%2C%22categories%3A%27modloader%27%22%2C%22categories%3A%27rift%27%22%5D%2C%5B%22project_type%3Amod%22%5D%5D", null,
-            { response ->
-                currentIndex += 20
-                lastIndex    += 20
+                drawerFragmentTransaction
+                    .replace(R.id.fragmentContainer, modsFragment)
+                    .commit()
 
-                Log.d("webCall", "requestComplete")
-
-                val data = format.decodeFromString<ModrinthSearchModel>(response.toString())
-
-                for ((currentPos, hit) in data.hits.withIndex()) {
-                    allData.add(hit)
-
-                    listAdapter.notifyItemInserted(currentPos)
-
-                    Log.d("webCall", "itemAdd ${hit.title}, ${hit.icon_url}")
-
-                }
-            },
-            { error ->
-                // TODO: Handle error
-            }
-        ) {
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String, String>()
-                headers["User-Agent"] = "Mozilla/5.0 (Linux; Android ${Build.VERSION.RELEASE}) Rithle/0.0.1 (github.com/TheClashFruit/Rithle; admin@theclashfruit.me) Fuel/2.3.1"
-                return headers
+                return true
             }
         }
 
-        RithleSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+        return false
     }
 }
---
+++ app/src/main/java/me/theclashfruit/rithle/fragments/ModsFragment.kt
@@ -0,0 +1,114 @@
+package me.theclashfruit.rithle.fragments
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import me.theclashfruit.rithle.R
+import me.theclashfruit.rithle.adapters.ModListAdapter
+import me.theclashfruit.rithle.classes.RithleSingleton
+import me.theclashfruit.rithle.models.ModrinthSearchHitsModel
+import me.theclashfruit.rithle.models.ModrinthSearchModel
+
+class ModsFragment : Fragment() {
+    private var currentIndex = 20
+    private var lastIndex    = 0
+
+    private var allData: ArrayList<ModrinthSearchHitsModel> = arrayListOf();
+    private var recyclerView: RecyclerView? = null;
+
+    private var listAdapter: ModListAdapter?        = null
+    private var layoutManager: LinearLayoutManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_mods, container, false)
+
+        recyclerView = rootView.findViewById(R.id.recyclerView)
+
+        listAdapter   = ModListAdapter(allData, requireContext())
+        layoutManager = LinearLayoutManager(requireContext())
+
+        recyclerView!!.layoutManager = layoutManager
+        recyclerView!!.adapter       = listAdapter
+
+        getItems(requireContext())
+
+        recyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if(!recyclerView.canScrollVertically(1))
+                    getItems(requireContext())
+
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+        })
+
+        return rootView
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ModsFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+
+    private fun getItems(context: Context) {
+        Log.d("webCall", "funCalled")
+
+        val format = Json { ignoreUnknownKeys = true }
+
+        val jsonObjectRequest = object : JsonObjectRequest(
+            Method.GET, "https://api.modrinth.com/v2/search?limit=${currentIndex}&offset=${lastIndex}&index=relevance&facets=%5B%5B%22categories%3A%27forge%27%22%2C%22categories%3A%27fabric%27%22%2C%22categories%3A%27quilt%27%22%2C%22categories%3A%27liteloader%27%22%2C%22categories%3A%27modloader%27%22%2C%22categories%3A%27rift%27%22%5D%2C%5B%22project_type%3Amod%22%5D%5D", null,
+            { response ->
+                currentIndex += 20
+                lastIndex    += 20
+
+                Log.d("webCall", "requestComplete")
+
+                val data = format.decodeFromString<ModrinthSearchModel>(response.toString())
+
+                for ((currentPos, hit) in data.hits.withIndex()) {
+                    allData.add(hit)
+
+                    listAdapter!!.notifyItemInserted(currentPos)
+
+                    Log.d("webCall", "itemAdd ${hit.title}, ${hit.icon_url}")
+
+                }
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0 (Linux; Android ${Build.VERSION.RELEASE}) Rithle/0.0.1 (github.com/TheClashFruit/Rithle; admin@theclashfruit.me) Fuel/2.3.1"
+                return headers
+            }
+        }
+
+        RithleSingleton.getInstance(context).addToRequestQueue(jsonObjectRequest)
+    }
+}
