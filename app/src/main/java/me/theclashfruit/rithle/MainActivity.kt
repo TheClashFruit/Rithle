8ec242c7fafe9b2ebdb24fe204150928eaee855d
TheClashFruit <clash.fruit123@gmail.com>
2022-12-02 18:39:11 +0100

    feat: use BottomNavbar instead of a Drawer

--- app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
+++ app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
@@ -2,71 +2,19 @@ package me.theclashfruit.rithle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.navigation.NavigationView
-import me.theclashfruit.rithle.fragments.ModsFragment
+import me.theclashfruit.rithle.fragments.HomeFragment
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var appBarLayout: AppBarLayout
-    private lateinit var toolBar: MaterialToolbar
-
-    private lateinit var navigationView: NavigationView
-    private lateinit var drawerLayout: DrawerLayout
-
-    private lateinit var drawerToggle: ActionBarDrawerToggle
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        appBarLayout = findViewById(R.id.appBarLayout)
-        toolBar      = findViewById(R.id.toolBar)
-
-        navigationView = findViewById(R.id.navigationView)
-        drawerLayout   = findViewById(R.id.drawerLayout)
-
-        setSupportActionBar(toolBar)
-
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setHomeButtonEnabled(true)
-
-        supportActionBar!!.subtitle = "Mods"
-
-        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.app_name, R.string.app_name)
-
-        drawerLayout.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener(this)
-
         val mainFragmentTransaction = supportFragmentManager.beginTransaction()
-
-        val modsFragment = ModsFragment.newInstance()
+        val homeFragment = HomeFragment.newInstance()
 
         mainFragmentTransaction
-            .replace(R.id.fragmentContainer, modsFragment)
+            .replace(R.id.parentFragmentContainer, homeFragment)
             .commit()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val drawerFragmentTransaction = supportFragmentManager.beginTransaction()
-
-        when(item.itemId) {
-            R.id.itemMods -> {
-                val modsFragment = ModsFragment.newInstance()
-
-                drawerFragmentTransaction
-                    .replace(R.id.fragmentContainer, modsFragment)
-                    .commit()
-
-                return true
-            }
-        }
-
-        return false
     }
 }
---
+++ app/src/main/java/me/theclashfruit/rithle/fragments/HomeFragment.kt
@@ -0,0 +1,43 @@
+package me.theclashfruit.rithle.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.theclashfruit.rithle.R
+
+class HomeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val mainFragmentTransaction = parentFragmentManager.beginTransaction()
+        val modsFragment = ModsFragment.newInstance()
+
+        mainFragmentTransaction
+            .replace(R.id.fragmentContainer, modsFragment)
+            .commit()
+
+        return rootView
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
