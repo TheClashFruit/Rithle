2a2c55e9d26588fa474a055a82324e018f2c2212
TheClashFruit <clash.fruit123@gmail.com>
2023-08-06 14:39:15 +0200

    feat: start setting up the drawer

---
+++ app/src/main/java/me/theclashfruit/rithle/fragments/HomeFragment.kt
@@ -0,0 +1,34 @@
+package me.theclashfruit.rithle.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.theclashfruit.rithle.R
+
+
+class HomeFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {}
+            }
+    }
+}
