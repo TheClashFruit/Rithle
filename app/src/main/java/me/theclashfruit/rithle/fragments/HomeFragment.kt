1325fab1efc696cccc3ec0243864616d6ecace0e
TheClashFruit <clash.fruit123@gmail.com>
2023-08-17 16:39:39 +0200

    feat: search stuff, tabs, more drawer things

--- app/src/main/java/me/theclashfruit/rithle/fragments/HomeFragment.kt
+++ app/src/main/java/me/theclashfruit/rithle/fragments/HomeFragment.kt
@@ -1,14 +1,23 @@
 package me.theclashfruit.rithle.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.search.SearchBar
 import me.theclashfruit.rithle.R
 
 
 class HomeFragment : Fragment() {
+
+    var searchBar: SearchBar? = null;
+    var navView: NavigationView? = null;
+    var navLayout: DrawerLayout? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -20,8 +29,48 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val inflatedView = inflater.inflate(R.layout.fragment_home, container, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        searchBar  = inflatedView.findViewById(R.id.search_bar)
+        navView    = inflatedView.findViewById(R.id.navView)
+        navLayout  = inflatedView.findViewById(R.id.drawerLayout)
+
+        searchBar!!.inflateMenu(R.menu.main_menu)
+        searchBar!!.setNavigationIcon(R.drawable.ic_menu)
+
+        searchBar!!.setOnMenuItemClickListener { menuItem -> true }
+
+        /*
+        val drawerToggle = ActionBarDrawerToggle(
+            requireActivity(),
+            navLayout,
+            toolBar,
+            R.string.app_name,
+            R.string.app_name
+        )
+        */
+
+        /*
+        navLayout!!.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        */
+
+        navView!!.setNavigationItemSelectedListener(::onNavigationItemSelected)
+
+        // drawerToggle.setHomeAsUpIndicator(R.drawable.ic_search)
+
+        return inflatedView
+    }
+
+    private fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.itemMinecraft -> {
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
     }
 
     companion object {
