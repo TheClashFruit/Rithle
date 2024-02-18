ee2e54d95da9812541ff74acaae8de6a367ec291
TheClashFruit <clash.fruit123@gmail.com>
2024-02-18 16:04:55 +0100

    feat, fix: notifications, fix auth

--- app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
+++ app/src/main/java/me/theclashfruit/rithle/MainActivity.kt
@@ -1,13 +1,19 @@
 package me.theclashfruit.rithle
 
 import android.accounts.AccountManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
@@ -26,8 +32,7 @@ import me.theclashfruit.rithle.modrinth.facets.Category
 import me.theclashfruit.rithle.modrinth.facets.FacetBuilder
 import me.theclashfruit.rithle.modrinth.facets.ProjectType
 import me.theclashfruit.rithle.onboarding.OnboardingActivity
-import java.text.NumberFormat
-import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -44,6 +49,40 @@ class MainActivity : AppCompatActivity() {
 
         searchBar = binding.searchBar;
         searchView = binding.searchView;
+
+        val sharedPref = getSharedPreferences("me.theclashfruit.rithle_preferences", Context.MODE_PRIVATE)
+
+        registerNotificationChannel(
+            "mr:project_update",
+            getString(R.string.project_update),
+            getString(R.string.project_update_desc),
+            3
+        )
+        registerNotificationChannel(
+            "mr:team_invite",
+            getString(R.string.team_invite),
+            getString(R.string.team_invite_desc),
+            4
+        )
+        registerNotificationChannel(
+            "mr:status_change",
+            getString(R.string.status_change),
+            getString(R.string.status_change_desc),
+            3
+        )
+        registerNotificationChannel(
+            "mr:moderator_message",
+            getString(R.string.moderator_message),
+            getString(R.string.moderator_message_desc),
+            5
+        )
+
+        if(AccountManager.get(this).getAccountsByType("me.theclashfruit.rithle").isEmpty()) {
+            Intent(this, OnboardingActivity::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }
 
         val auth = AccountManager.get(this).getAuthToken(AccountManager.get(this).getAccountsByType("me.theclashfruit.rithle")[0], "pat", null, this, null, null)
 
@@ -65,6 +104,39 @@ class MainActivity : AppCompatActivity() {
                 loadImage(it.avatarUrl)
             }
         }
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (!isGranted) {
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("Notifications")
+                        .setMessage("Rithle requires notification permissions in order to send you notifications.\n\nDismissing this dialog will dismiss it forever.")
+                        .setNegativeButton("Dismiss") { _, _ ->
+                            sharedPref
+                                .edit()
+                                .putBoolean("notifications_perms", false)
+                                .apply()
+                        }
+                        .setPositiveButton("Change") { _, _ ->
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            val uri = Uri.fromParts("package", packageName, null)
+                            intent.setData(uri)
+                            startActivity(intent)
+                        }
+                        .setOnDismissListener {
+                            sharedPref
+                                .edit()
+                                .putBoolean("notifications_perms", false)
+                                .apply()
+                        }
+                        .show()
+                }
+            }
+
+        if(sharedPref.getBoolean("notifications_perms", true) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
 
         searchBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
@@ -233,5 +305,14 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch(Dispatchers.Main){
             searchBar.menu.findItem(R.id.profile).icon = resource
         }
+    }
+
+    private fun registerNotificationChannel(id: String, name: String, description: String, importance: Int) {
+        val notificationChannel = NotificationChannel(id, name, importance)
+
+        notificationChannel.description = description
+
+        (getSystemService(NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(notificationChannel)
+
     }
 }
